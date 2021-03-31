08ea110713b07bebe1977a1d9d72dc23d779df3c
HyeonJi Tak <jirae1997@naver.com>
2021-04-01 07:45:23 +0900

    리스트 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'src/random_list.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    final randomWord = WordPair.random();
 
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title : Text("naming app"),
-        ),
-        body : Center(child: Text(randomWord.asPascalCase , textScaleFactor: 1.5,)),
-      ),
+      home: RandomList()
     );
   }
 }
